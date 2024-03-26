562afe426980a753ad470a756d3a5fbac3d77004
Mukesh Kumar <mukeshkmr7544@gmail.com>
2024-03-26 11:43:06 +0530

    initialized upload with few statements

--- BasicPythonProgram.py
+++ BasicPythonProgram.py
@@ -1,14 +1,14 @@
-# print("hello world")
+print("hello world")
 
-# #Write a program to accept two numbers from the user and calculate multiplication
-# num1 = int(input("enter num1 : "))
-# num2 = int(input("enter num2 : "))
+#Write a program to accept two numbers from the user and calculate multiplication
+num1 = int(input("enter num1 : "))
+num2 = int(input("enter num2 : "))
 
-# print(num1*num2)
+print(num1*num2)
 
 
-# #Display three string “Name”, “Is”, “James” as “Name**Is**James”
-# print('my','Name','is','james',sep='**')
+#Display three string “Name”, “Is”, “James” as “Name**Is**James”
+print('my','Name','is','james',sep='**')
 
 num = 8
 print('%o' % num)
